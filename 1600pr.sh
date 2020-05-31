0ec37dbdbc77f09dedb2d32626a204de82340911
Anders Jensen-Urstad <anders@unix.se>
2020-05-31 20:54:11 +0200

    Add initial version

---
+++ 1600pr.sh
@@ -0,0 +1,394 @@
+#!/bin/sh
+#
+# Simple static photoblog generator.
+# Requires ImageMagick for creation of thumbnails/alternative sizes, but you can
+# turn that off.
+#
+# Example usage:
+#
+# ./1600pr.sh ~/blargh.jpg
+#
+# Anders Jensen-Urstad <anders@unix.se>
+# License: MIT
+
+site_title=${_1600PR_SITE_TITLE:-"J. Random Photoblogger"}
+site_url=${_1600PR_SITE_URL:-"https://example.com/"} # Absolute URL to photoblog; used for RSS
+email=${_1600PR_EMAIL:-"foobar@example.com"}
+
+db_file="./_1600pr.dat"
+image_dir="./images"
+output_dir="./public"
+web_root_path="/" # Relative URL to site, e.g. / or /photoblog/; this affects links
+menu="<a href=\"${web_root_path}\">home</a> <a href=\"${web_root_path}photo\">archive</a> <a href=\"mailto:${email}\">contact</a>"
+archive_page=true # Whether to create an archive page and generate thumbs. If false, also change menu.
+responsive=true # Create different sizes of each image (see sizes) for responsive images
+sizes="1920 1600 1280 800" # Image sizes to create (widths)
+
+###########################################################################
+
+rebuild=false
+post_date=$(date "+%a, %d %b %Y %H:%M:%S %z")
+
+usage() {
+  echo "Usage:"
+  echo "  $0 [-d <date>] [-t <title>] [-n] <image to add>"
+  echo "  $0 -r <id to remove>"
+  echo "  $0 [-h] [-b]"
+  echo ""
+  echo " -d <date>   Publication date (for RSS) formatted as RFC 822 datetime,"
+  echo "             e.g. Sat, 07 Sep 2002 0:00:01 GMT."
+  echo "             If not specified, current date will be used."
+  echo " -t <title>  Image title. If not specified, publication date will be used as"
+  echo "             title in RSS feed."
+  echo " -b          Rebuild everything to ${output_dir} (image versions, pages)."
+  echo " -r <id>     Remove post with specified ID from database and rebuild."
+  echo "             Doesn't remove image from [non-public] ${image_dir} or from"
+  echo "             ${output_dir}."
+  echo " -h          Show help message and exit."
+}
+
+while getopts "hbd:t:r:" opt; do
+  case $opt in
+    h) usage; exit;;
+    b) rebuild=true ;;
+    d) post_date="${OPTARG}" ;;
+    t) POST_TITLE="${OPTARG}" ;;
+    r) ID_TO_REMOVE="${OPTARG}" ;;
+    *) usage; exit;; 
+  esac
+done
+
+get_num_posts () { sed '/^\s*$/d' "${db_file}" | wc -l ; }
+get_latest_id () { tail -n1 "${db_file}" | awk '{print $1}' ; }
+get_new_id () {
+  highest_id=$(awk '{print $1}' "${db_file}" | sort -n | tail -n1)
+  echo $((highest_id + 1))
+}
+# $1: post id
+get_title () {
+  title=$(grep "^${post_id}[[:blank:]]" "${db_file}" \
+    | awk 'BEGIN {FS="\t"}; {print $4}' \
+    | sed 's/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g'
+    )
+  date=$(grep "^${post_id}[[:blank:]]" "${db_file}" | awk 'BEGIN {FS="\t"}; {print $2}')
+  if [ -n "${title}" ]; then
+    echo "${title}"
+  else
+    echo "${date}"
+  fi
+}
+
+# $1: image filename, $2: post id
+gen_thumb () {
+  if [ ! -f "${output_dir}/images/${2}/${1}.thumb.jpg" ]; then
+    echo "Generating thumbnail for ${1}"
+    convert -define jpeg:size=100x65 "${image_dir}/${1}" -thumbnail 100x65^ -gravity center -extent 100x65 "${output_dir}/images/${2}/${1}.thumb.jpg"
+    mogrify -unsharp 0.25x0.08+8.3+0.045 "${output_dir}/images/${2}/${1}.thumb.jpg"
+  fi
+}
+
+# $1: image filename, $2: post id
+gen_image_versions () {
+  # unsharp values from https://www.smashingmagazine.com/2015/06/efficient-image-resizing-with-imagemagick/
+  for size in ${sizes}; do
+    if [ ! -f "${output_dir}/images/${2}/${1}.${size}.jpg" ]; then
+      echo "Generating ${size}x version of ${1}"
+      convert -resize "${size}x" -quality 85 "${image_dir}/${1}" "${output_dir}/images/${2}/${1}.${size}.jpg"
+      mogrify -unsharp 0.25x0.08+8.3+0.045 "${output_dir}/images/${2}/${1}.${size}.jpg"
+    fi
+  done
+}
+
+# $1: image filename, $2: post id
+gen_images () {
+  mkdir -p "${output_dir}/images/${2}"
+  if [ "${responsive}" = true ]; then
+    gen_image_versions "${1}" "${2}"
+  else
+    cp "${image_dir}/${1}" "${output_dir}/images/${2}"
+  fi
+  gen_thumb "${1}" "${2}"
+}
+
+# $1: path, $2: RFC 822 datetime, $3: optional title
+add_image () {
+  orig_file_path=$1
+  orig_file_basename=$(basename "${orig_file_path}")
+  post_date=$2
+  post_title=$3
+  echo "Adding ${orig_file_basename}"
+
+  if [ ! -f "${orig_file_path}" ]; then
+    echo "Error: File ${orig_file_path} does not exist."
+    exit 1
+  fi
+
+  # If filename already exists, prepend timestamp to the new one
+  if [ -f "${image_dir}/${orig_file_basename}" ]; then
+    timestamp=$(date "+%s")
+    file_basename="${timestamp}_${orig_file_basename}"
+    echo "${orig_file_basename} already exists; using ${file_basename} instead"
+  else
+    file_basename="${orig_file_basename}"
+  fi
+
+  # Figure out what ID to use
+  prev_id=$(get_latest_id)
+  if [ -z "${prev_id}" ]; then
+    new_id=1
+  else
+    new_id=$(get_new_id)
+  fi
+
+  # Write to db
+  printf "%s\t%s\t%s\t%s\n" "${new_id}" "${post_date}" "${file_basename}" "${post_title}" >> "${db_file}"
+  # Copy original to (non-public) image directory
+  cp -p "${orig_file_path}" "${image_dir}/${file_basename}"
+  # Create different sizes/thumb
+  gen_images "${file_basename}" "${new_id}"
+}
+
+# $1: filename, $2: post id
+generate_img_src () {
+  title=$(get_title "${2}")
+  if [ "${responsive}" = true ]; then
+    for size in $sizes; do
+      srcset="${srcset}${web_root_path}images/${2}/${1}.${size}.jpg ${size}w, "
+    done
+    srcset=${srcset%, } # remove trailing ", "
+    largest=${sizes%% *}
+    echo "<img src=\"${web_root_path}images/${2}/${1}.${largest}.jpg\" srcset=\"${srcset}\" sizes=\"100vw\" class=\"image-inner\" alt=\"${title}\">"
+  else
+    echo "<img src=\"${web_root_path}images/${2}/${1}\" class=\"image-inner\" alt=\"${title}\">"
+  fi
+}
+
+# $1: post id
+generate_post_html () {
+  unset prev_html next_html
+
+  post_id=${1}
+  prev_id=$(grep -B1 "^${post_id}[[:blank:]]" "${db_file}" | head -n1 | grep -v "^${post_id}[[:blank:]]" | awk '{print $1}')
+  next_id=$(grep -A1 "^${post_id}[[:blank:]]" "${db_file}" | tail -n1 | grep -v "^${post_id}[[:blank:]]" | awk '{print $1}')
+  filename=$(grep "^${post_id}[[:blank:]]" "${db_file}" | awk 'BEGIN {FS="\t"}; {print $3}')
+
+  if [ -n "${prev_id}" ]; then
+    image_before="<a href=\"${web_root_path}photo/${prev_id}/\">"
+    image_after="</a>"
+    prev_html="<a class=\"bold\" href=\"${web_root_path}photo/${prev_id}/\">← older</a> " 
+  fi
+
+  if [ -n "${next_id}" ]; then
+    next_html=" <a class=\"bold\" href=\"${web_root_path}photo/${next_id}/\">newer →</a>" 
+  fi
+
+  mkdir -p "${output_dir}/photo/${post_id}"
+
+  cat <<EOF > "${output_dir}/photo/${post_id}/index.html"
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="referrer" content="no-referrer">
+    <title>${site_title}</title>
+    <link rel="stylesheet" href="${web_root_path}style.css">
+    <link href="${web_root_path}index.xml" rel="alternate" type="application/rss+xml" title="${site_title}">
+  </head>
+  <body>
+
+  ${image_before}
+    <div class="image-outer">$(generate_img_src "${filename}" "${post_id}")</div>
+  ${image_after}
+  ${prev_html}
+  ${menu}
+  ${next_html}
+  </body>
+</html>
+EOF
+}
+
+gen_archive_html () {
+  echo "Creating archive page"
+  thumb_html=$(sed '1!G;h;$!d' "${db_file}" \
+    | awk -v web_root=${web_root_path} \
+      'BEGIN { FS="\t" }; {
+        printf "<a href=\"%sphoto/%s\"><img src=\"%simages/%s/%s.thumb.jpg\" alt=\"%s\"></a>\n",
+        web_root, $1, web_root, $1, $3, $2
+      }'
+    )
+
+  cat <<EOF > "${output_dir}/photo/index.html"
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="referrer" content="no-referrer">
+    <title>${site_title}</title>
+    <link rel="stylesheet" href="${web_root_path}style.css">
+  </head>
+  <body>
+    <div class="list">
+      ${thumb_html}
+    </div>
+    ${menu}
+  </body>
+</html>
+EOF
+}
+
+gen_css () {
+  cat <<EOF > "${output_dir}/style.css"
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background: black;
+  color: white;
+  text-align: center;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+a {
+  color: #868e96;
+  font-family: sans-serif;
+  font-size: 0.8em;
+}
+
+.image-outer {
+  display: grid;
+  height: 100%;
+}
+
+.image-inner {
+  max-width: 100%;
+  max-height: 100vh;
+  margin: auto;
+}
+EOF
+}
+
+gen_rss() {
+  last_build_date=$(date "+%a, %d %b %Y %H:%M:%S %z")
+  rss_items=$(sed '1!G;h;$!d' "${db_file}" \
+    | tail -n10 \
+    | awk -v site_url="${site_url}" -v largest="${sizes%% *}" \
+      'BEGIN { FS="\t" }; {
+        printf "<item>\n"
+        if (length($4) > 0) {
+          printf "<title>%s</title>\n", $4
+        } else {
+          printf "<title>%s</title>\n", $2
+        }
+        printf "<link>%sphoto/%s</link>\n", site_url, $1
+        printf "<pubDate>%s</pubDate>\n", $2
+        printf "<guid>%sphoto/%s</guid>\n", site_url, $1
+        printf "<description><![CDATA[<img src=\"%simages/%s/%s.%s.jpg\" />]]></description>\n", site_url, $1, $3, largest
+        printf "</item>\n"
+      }'
+    )
+
+  cat <<EOF > "${output_dir}/index.xml"
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>${site_title}</title>
+    <link>${site_url}</link>
+    <description>Recent photos from ${site_title}</description>
+    <lastBuildDate>${last_build_date}</lastBuildDate>
+    <atom:link href="${site_url}index.xml" rel="self" type="application/rss+xml" />
+    ${rss_items}
+  </channel>
+</rss>
+EOF
+}
+
+rebuild_all () {
+  echo "Rebuilding everything"
+  num_posts=$(get_num_posts)
+  if [ "${num_posts}" -lt "1" ]; then
+    echo "No posts in ${db_file}. Exiting."
+    exit
+  fi
+
+  while IFS= read -r line; do
+    if [ -n "${line}" ]; then
+      post_id=$(echo "${line}" | awk '{print $1}')
+      filename=$(echo "${line}" | awk 'BEGIN {FS="\t"}; {print $3}')
+      echo "Processing ${filename} (ID ${post_id})"
+      gen_images "${filename}" "${post_id}"
+      generate_post_html "${post_id}"
+    fi
+  done < "${db_file}"
+
+  if [ "${archive_page}" = true ]; then
+    gen_archive_html
+  fi
+  echo "Copying latest post to ${output_dir}/index.html"
+  cp "${output_dir}/photo/$(get_latest_id)/index.html" "${output_dir}/index.html"
+  gen_css
+}
+
+# $1: ID to remove
+remove_post () {
+  echo "Removing ID ${1}"
+  if grep -q "^${1}[[:blank:]]" "${db_file}"; then
+    grep -v "^${1}[[:blank:]]" "${db_file}" > "${db_file}.tmp" && mv "${db_file}.tmp" "${db_file}" 
+  else
+    echo "ID ${1} not found in db. Exiting."
+    exit 1
+  fi
+}
+
+# Make sure necessary files/directories exist
+touch "${db_file}"
+mkdir -p "${image_dir}" "${output_dir}" "${output_dir}/images" "${output_dir}/photo"
+if ! [ -f "${output_dir}/style.css" ]; then
+  echo "${output_dir}/style.css missing. Generating"
+  gen_css
+fi
+
+if [ "${rebuild}" = true ]; then
+  rebuild_all
+elif [ -n "${ID_TO_REMOVE}" ]; then
+  if [ -z "${ID_TO_REMOVE##*[!0-9]*}" ]; then
+    echo "Specified ID not positive integer."
+    exit 1
+  else
+    remove_post "${ID_TO_REMOVE}"
+    rebuild_all
+  fi
+else
+  shift $((OPTIND - 1))
+  if [ -z "$1" ]; then
+    usage
+    exit 1
+  fi
+
+  add_image "${1}" "${post_date}" "${POST_TITLE}"
+  new_id=$(get_latest_id)
+  generate_post_html "${new_id}"
+  # New post also becomes the new index.html
+  cp "${output_dir}/photo/${new_id}/index.html" "${output_dir}/index.html"
+
+  # If there's more than one post, update the previous one's HTML so it gets a "Next" link to the new post
+  num_posts=$(get_num_posts)
+  if [ "${num_posts}" -gt "1" ]; then
+    prev_id=$(tail -n2 "${db_file}" | head -n1 | awk '{print $1}')
+    generate_post_html "${prev_id}"
+  fi
+
+  if [ "${archive_page}" = true ]; then
+    gen_archive_html
+  fi
+
+  gen_rss
+fi
+
+echo "All done! Your photoblog is in ${output_dir}"
---
+++ README.md
@@ -0,0 +1,115 @@
+1600pr.sh: simple static photoblog generator
+============================================
+1600pr.sh is a minimalist static photoblog generator made up of a single
+POSIX-compliant shell script. It should work on most Unix-like systems. It passes
+[ShellCheck](https://github.com/koalaman/shellcheck) and some effort has been
+made to ensure external tools (grep, sed, awk, etc.) are used in a portable
+way. The only external dependency is [ImageMagick](https://imagemagick.org)
+for creation of thumbnails and alternative sizes (for responsive images),
+however this can be disabled if you don't want those things.
+
+You can see it in use on [minorshadows.net](https://minorshadows.net/).
+
+Named after Fujifilm Neopan 1600 Professional (RIP). Inspired (but not based
+on) [Expose](https://github.com/Jack000/Expose).
+
+How to install and use
+----------------------
+1. Download 1600pr.sh. Make it executable (`chmod +x 1600pr.sh`).
+
+2. Make sure ImageMagick is installed, *or* set `responsive` and `archive_page`
+to `false` in `1600pr.sh`.
+
+3. Add image: `./1600pr.sh ~/some-image.jpg`
+
+Boom, you now have a photoblog built in `public/`. rsync or scp it somewhere.
+You probably want to change a few variables (title, absolute url, menu) in
+the script, though, or set environment variables (`_1600PR_SITE_TITLE`,
+`_1600PR_SITE_URL`, `_1600PR_EMAIL`).
+
+Add image with title:
+
+    ./1600pr.sh -t "My cat." ~/my-cat.jpg
+
+Title is only used in the RSS feed, and for the image's `alt` attribute. If no
+title is set, current date will be used instead.
+
+Add image with title and custom date (should be RFC 822 datetime):
+
+    ./1600pr.sh -t "My cat." -d "Sun, 31 May 2020 19:22:17 +0200" ~/my-cat.jpg
+
+If you nuked your `public/`, or changed something in the script (e.g. what
+sizes to generate), you can rebuild everything with -b:
+
+    ./1600pr.sh -b
+
+Remove post with ID 23 from the "database" and rebuild:
+
+    ./1600pr.sh -d 23
+
+(This doesn't *remove* anything from `public/`, nor does it remove the
+original image stored in `images/`.)
+
+How it works
+------------
+Information about each post is stored in `_1600pr.dat`. It's a plaintext file
+where each line represents one post, in ascending order from oldest to newest.
+(To change the order, just move lines around and rebuild.)
+Each line has a tab-separated values: image ID, date, filename, optional title.
+
+The original image is copied to the non-public folder `images/`. For each photo,
+a number of sizes are generated (unless you've opted not to) and put in
+`public/images/<id>/`.
+
+One page per photo. Each photo page gets the relative URL `/photo/<id>/`.
+Each page has "prev" and "next" links, if applicable.
+
+`/photo/` is an archive page with thumbnails of every photo.
+
+The most recent photo page is copied to `/index.html`.
+
+RSS and CSS files are also generated.
+
+Starting with only `1600pr.sh` and adding two photos (foo01.jpg and foo02.jpg),
+we get the following:
+
+```
+.
+├── _1600pr.db
+├── 1600pr.sh
+├── images
+│   ├── foo01.jpg
+│   └── foo02.jpg
+└── public
+    ├── images
+    │   ├── 1
+    │   │   ├── foo01.jpg.1280.jpg
+    │   │   ├── foo01.jpg.1600.jpg
+    │   │   ├── foo01.jpg.1920.jpg
+    │   │   ├── foo01.jpg.800.jpg
+    │   │   └── foo01.jpg.thumb.jpg
+    │   └── 2
+    │       ├── foo02.jpg.1280.jpg
+    │       ├── foo02.jpg.1600.jpg
+    │       ├── foo02.jpg.1920.jpg
+    │       ├── foo02.jpg.800.jpg
+    │       └── foo02.jpg.thumb.jpg
+    ├── index.html
+    ├── index.xml
+    ├── photo
+    │   ├── 1
+    │   │   └── index.html
+    │   ├── 2
+    │   │   └── index.html
+    │   └── index.html
+    └── style.css
+
+```
+
+Customizing
+-----------
+A few things (such as what sizes to generate) can be set with variables at
+the top of the script. Otherwise just hack it to your heart's content. This
+is a quick hack for my own personal use: it's deliberately minimalist and I'm
+not looking to add features, but perhaps it can be of use to someone else too.
+
